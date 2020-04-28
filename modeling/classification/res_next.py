71992cbdedf444391fd564c2ce835a24acc6ffaa
Tao Mingyang <mingyang.tao@nusri.cn>
2020-04-28 10:30:55 +0800

    add res_next.py

---
+++ modeling/classification/res_next.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+# Version:		python3.6
+# Description:	
+# Author:		bqrmtao@qq.com
+# date:			2020/4/28 10:29
+
+from __future__ import absolute_import
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
