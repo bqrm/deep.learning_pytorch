42244364ee57b9140bdacfa0fa296f1136b8802f
Tao Mingyang <mingyang.tao@nusri.cn>
2020-04-29 22:58:24 +0800

    correct misundersdanding according to torchvision.models.resnet.py

--- modeling/classification/res_net.py
+++ modeling/classification/res_net.py
@@ -14,63 +14,77 @@ import torch.nn as nn
 class BasicBlock(nn.Module):
     expansion = 1
 
-    def __init__(self, in_plane, out_plane, stride=1):
+    def __init__(self, in_channel, out_channel, stride=1):
         super(BasicBlock, self).__init__()
 
-        self.conv_1 = nn.Conv2d(in_plane, out_plane, 3, stride=stride, padding=1, bias=False)
-        self.bn_1 = nn.BatchNorm2d(out_plane)
-        self.conv_2 = nn.Conv2d(out_plane, out_plane, 3, padding=1, bias=False)
-        self.bn_2 = nn.BatchNorm2d(out_plane)
+        self.relu = nn.ReLU(inplace=True)
 
-        if stride != 1 or in_plane != self.expansion*out_plane:
+        self.conv_1 = nn.Conv2d(in_channel, out_channel, 3, stride=stride, padding=1, bias=False)
+        self.bn_1 = nn.BatchNorm2d(out_channel)
+        self.conv_2 = nn.Conv2d(out_channel, out_channel, 3, padding=1, bias=False)
+        self.bn_2 = nn.BatchNorm2d(out_channel)
+
+        if stride != 1 or in_channel != self.expansion*out_channel:
             self.shortcut = nn.Sequential(
-                nn.Conv2d(in_plane, self.expansion*out_plane, 1, stride=stride, bias=False),
-                nn.BatchNorm2d(self.expansion*out_plane),
+                nn.Conv2d(in_channel, self.expansion*out_channel, 1, stride=stride, bias=False),
+                nn.BatchNorm2d(self.expansion*out_channel),
             )
         else:
             self.shortcut = None
 
     def forward(self, x):
-        output = nn.functional.relu(self.bn_1(self.conv_1(x)))
-        output = self.bn_2(self.conv_2(output))
+        output = self.conv_1(x)
+        output = self.bn_1(output)
+        output = self.relu(output)
+
+        output = self.conv_2(output)
+        output = self.bn_2(output)
+
         if self.shortcut is None:
             output += x
         else:
             output += self.shortcut(x)
-        output = nn.functional.relu(output)
+
+        output = self.relu(output)
+
         return output
 
 
 class BottleNeck(nn.Module):
     expansion = 4
 
-    def __init__(self, in_plane, out_plane, stride=1):
+    def __init__(self, in_channel, out_channel, stride=1, shortcut=None, base_width=64):
         super(BottleNeck, self).__init__()
 
-        self.conv_1 = nn.Conv2d(in_plane, out_plane, 1, bias=False)
-        self.bn_1 = nn.BatchNorm2d(out_plane)
-        self.conv_2 = nn.Conv2d(out_plane, out_plane, 3, stride=stride, padding=1, bias=False)
-        self.bn_2 = nn.BatchNorm2d(out_plane)
-        self.conv_3 = nn.Conv2d(out_plane, self.expansion*out_plane, 1, bias=False)
-        self.bn_3 = nn.BatchNorm2d(out_plane)
+        self.relu = nn.ReLU(inplace=True)
 
-        if stride != 1 or in_plane != self.expansion*out_plane:
-            self.shortcut = nn.Sequential(
-                nn.Conv2d(in_plane, self.expansion*out_plane, 1, stride=stride, bias=False),
-                nn.BatchNorm2d(self.expansion*out_plane),
-            )
-        else:
-            self.shortcut = None
+        mid_channel = int(out_channel * (base_width / 64.))
+        self.conv_1 = nn.Conv2d(in_channel, mid_channel, 1, bias=False)
+        self.bn_1 = nn.BatchNorm2d(mid_channel)
+        self.conv_2 = nn.Conv2d(mid_channel, mid_channel, 3, stride=stride, padding=1, bias=False)
+        self.bn_2 = nn.BatchNorm2d(mid_channel)
+        self.conv_3 = nn.Conv2d(mid_channel, self.expansion*out_channel, 1, bias=False)
+        self.bn_3 = nn.BatchNorm2d(self.expansion*out_channel)
+        self.shortcut = shortcut
 
     def forward(self, x):
-        output = nn.functional.relu(self.bn_1(self.conv_1(x)))
-        output = nn.functional.relu(self.bn_2(self.conv_2(output)))
-        output = self.bn_3(self.conv_3(output))
+        output = self.conv_1(x)
+        output = self.bn_1(output)
+        output = self.relu(output)
+
+        output = self.conv_2(output)
+        output = self.bn_2(output)
+        output = self.relu(output)
+
+        output = self.conv_3(output)
+        output = self.bn_3(output)
+
         if self.shortcut is None:
             output += x
         else:
             output += self.shortcut(x)
-        output = nn.functional.relu(output)
+
+        output = self.relu(output)
         return output
 
 
@@ -78,36 +92,53 @@ class ResNet(nn.Module):
     def __init__(self, block, num_blocks, num_class=10):
         super(ResNet, self).__init__()
 
-        self.in_plane = 64
+        self.in_channel = 64
 
-        self.conv_1 = nn.Conv2d(3, 64, 3, padding=1, bias=False)
-        self.bn_1 = nn.BatchNorm2d(64)
+        self.conv_1 = nn.Conv2d(3, self.in_channel, 3, padding=1, bias=False)
+        self.bn_1 = nn.BatchNorm2d(self.in_channel)
+        self.relu = nn.ReLU(inplace=True)
+        self.maxpool = nn.MaxPool2d(3, stride=2, padding=1)
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+
         self.layer_1 = self._make_layer(block, 64, num_blocks[0], stride=1)
         self.layer_2 = self._make_layer(block, 128, num_blocks[1], stride=2)
         self.layer_3 = self._make_layer(block, 256, num_blocks[2], stride=2)
         self.layer_4 = self._make_layer(block, 512, num_blocks[3], stride=2)
         self.linear = nn.Linear(512*block.expansion, num_class)
 
-    def _make_layer(self, block, out_plane, num_block, stride):
-        strides = [stride] + [1] * (num_block - 1)
-        layers = []
+    def _make_layer(self, block, out_channel, num_block, stride):
+        shortcut = None
+        if stride != 1 or self.in_channel != block.expansion*out_channel:
+            shortcut = nn.Sequential(
+                nn.Conv2d(self.in_channel, block.expansion*out_channel, 1, stride=stride, bias=False),
+                nn.BatchNorm2d(block.expansion*out_channel),
+            )
 
-        for tmp_stride in strides:
-            layers.append(block(self.in_plane, out_plane, tmp_stride))
-            self.in_plane = out_plane * block.expansion
+        layers = list()
+        layers.append(block(self.in_channel, out_channel, stride, shortcut))
+        self.in_channel = out_channel * block.expansion
+
+        for _ in range(1, num_block):
+            layers.append(block(self.in_channel, out_channel))
 
         return nn.Sequential(*layers)
 
     def forward(self, x):
-        output = nn.functional.relu(self.bn_1(self.conv_1(x)))
-        output = self.layer_1(output)
-        output = self.layer_2(output)
-        output = self.layer_3(output)
-        output = self.layer_4(output)
-        output = nn.functional.avg_pool2d(output, 4, padding=2)
-        output = output.view(output.shape[0], -1)
-        output = self.linear(output)
-        return output
+        x = self.conv_1(x)
+        x = self.bn_1(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+
+        x = self.layer_1(x)
+        x = self.layer_2(x)
+        x = self.layer_3(x)
+        x = self.layer_4(x)
+
+        x = self.avgpool(x)
+        x = torch.flatten(x, 1)
+        x = self.linear(x)
+
+        return x
 
 
 def res_net_18():
@@ -129,6 +160,22 @@ def res_net_101():
 def res_net_152():
     return ResNet(BottleNeck, [3, 8, 36, 3])
 
+
+def test(model, criterion, data_loader, use_cuda=False):
+    pass
+
+
+def train(model, epoch, criterion, optimizer, data_loader, use_cuda=False):
+    pass
+
+
+def main():
+    import argparse
+    model = res_net_50()
+
+
+if "__main__" == __name__:
+    main()
 
 
 
